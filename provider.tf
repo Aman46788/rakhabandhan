c5d3a1b8d9ed00e918c513f6ecf27e0bd44cb12b
aman46788 <amanchauhan1182@gmail.com>
2025-08-11 11:19:30 +0530

    added backend file

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.31.0"
+    }
+  }
+  backend "azurerm" {}
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "661ea49e-05d4-429e-8204-de0d46c539b5"
+}
